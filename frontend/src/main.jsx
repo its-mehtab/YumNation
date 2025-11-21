08453af7710b15e5e841126612250eb0fc0c35c6
Mehtab Alam <mehtabalam0908@gmail.com>
2025-11-21 19:37:28 +0530

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import Header from "./components/Header/Header.jsx";
+import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
