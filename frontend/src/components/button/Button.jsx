29fd5a80c5b65f31047a097af23cb0086067a357
Mehtab Alam <mehtabalam0908@gmail.com>
2025-11-21 20:30:44 +0530

    add breadcumb

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -6,13 +6,13 @@ const Button = ({
   btnColor = "bg-[#FB9300] text-white hover:bg-[#027a36]",
   addClass = "",
 }) => (
-  <a
+  <div
     href="#"
     className={`inline-flex gap-2.5 font-[bangers] px-10 py-4.5 rounded-xl shadow-[2px_2px_0px_2px_rgba(0,0,0)] transition-all duration-300 ease-in-out ${btnColor} ${addClass}`}
   >
     {btnName}
     <BtnArrowIcon />
-  </a>
+  </div>
 );
 
 export default Button;
--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   return (
     <section
-      className="bg-cover bg-center h-screen flex items-center justify-center"
+      className="bg-cover bg-center  pt-34 pb-28"
       style={{ backgroundImage: `url(${assets.bannerBg})` }}
     >
-      <h1 className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-white text-[210px]">
-        Shop
-      </h1>
+      <div className="mx-auto max-w-[1340px] px-4 sm:px-6 lg:px-8">
+        <h1 className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 text-white text-[210px] text-center">
+          Shop
+        </h1>
+        <ul className="flex gap-2 justify-center">
+          <li>
+            <Link href="/" className="text-xl font-semibold text-white">
+              Home -
+            </Link>
+          </li>
+          <li className="text-xl text-white">Shop</li>
+        </ul>
+      </div>
     </section>
   );
 };
